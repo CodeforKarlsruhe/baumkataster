0b12854a174d0d605364418d272ca978ca48f337
Andreas Kugel <andreas.kugel@web.de>
2020-05-18 18:48:50 +0200

    kataster field information

--- tools/readKataster.py
+++ tools/readKataster.py
@@ -1,5 +1,31 @@
 import geopandas as gp
 
+# documentation for reading kataster file is here:
+# https://geoportal.karlsruhe.de/server/rest/services/Fachplaene/Baumkataster/MapServer/1
+
+##Layer: Bäume (Zoomstufe 16 bis 18) (ID: 1)
+##Name: Bäume (Zoomstufe 16 bis 18)
+##Display Field: ARTDEUT
+##Type: Feature Layer
+##Geometry Type: esriGeometryPoint
+##Supported Query Formats: JSON, AMF, geoJSON
+##HTML Popup Type: esriServerHTMLPopupTypeAsHTMLText
+
+##Fields:
+##
+##    OBJECTID ( type: esriFieldTypeOID , alias: OBJECTID )
+##    LFDBNR ( type: esriFieldTypeInteger , alias: LFDBNR )
+##    LFDNR ( type: esriFieldTypeInteger , alias: LFDNR )
+##    ARTDEUT ( type: esriFieldTypeString , alias: ARTDEUT , length: 255 )
+##    ARTLAT ( type: esriFieldTypeString , alias: ARTLAT , length: 100 )
+##    BAUMGRUPPE ( type: esriFieldTypeString , alias: BAUMGRUPPE , length: 100 )
+##    HTML ( type: esriFieldTypeString , alias: HTML , length: 4000 )
+##    SHAPE ( type: esriFieldTypeGeometry , alias: SHAPE )
+##
+
+
+
+
 baumfile = "baumfile.json"
 baumtable = "baumtable.csv"
 
