c47a9c6cf0a29e3f8e881645e9cedc65f1e28603
Andreas Kugel <andreas.kugel@web.de>
2025-03-11 21:44:05 +0100

    readKataster iterating over new url, 2025

--- tools/readKataster.py
+++ tools/readKataster.py
@@ -1,4 +1,5 @@
 import geopandas as gp
+import pandas as pd
 
 # documentation for reading kataster file is here:
 # https://geoportal.karlsruhe.de/server/rest/services/Fachplaene/Baumkataster/MapServer/1
@@ -26,12 +27,36 @@ import geopandas as gp
 
 
 
-baumfile = "baumfile.json"
-baumtable = "baumtable.csv"
+baumfile = "baumfile-20250311.json"
+baumtable = "baumtable-20250311.csv"
 
 # new 2024
 url = "https://geoportal.karlsruhe.de/server/rest/services/Fachplaene/Baumkataster/MapServer/1/query?where=STADTTEIL+IS+NOT+NULL&outFields=LFDBNR%2CARTDEUT%2CARTLAT%2CBAUMART_ALLGEMEIN%2CBAUMGRUPPE%2CSTADTTEIL&returnGeometry=true&f=geojson"
-b = gp.read_file(url)
+
+url = "https://geoportal.karlsruhe.de/ags04/rest/services/Hosted/Baumkataster/FeatureServer/2/query?where=lfdbnr+IS+NOT+NULL&outFields=artdeut%2Cartlat%2Cbaumgruppe%2Clfdnr%2Clfdbnr%2Cbaumart_allgemein&returnGeometry=true&f=geojson"
+
+offset = 0
+all_data = []
+
+while True:
+    print("Reading offset ",offset)
+    url = f"https://geoportal.karlsruhe.de/ags04/rest/services/Hosted/Baumkataster/FeatureServer/2/query?where=1%3D1&outFields=*&resultOffset={str(offset)}&resultRecordCount=1000&orderByFields=objectid&returnGeometry=true&f=geojson"
+    print("Reading offset ",offset,url)
+    data = gp.read_file(url)
+    
+    if data.empty:
+        break
+    
+    print("Read ",len(data))
+    all_data.append(data)
+    offset += 1000
+    
+    if offset > 200000:
+        break
+
+b = gp.GeoDataFrame(pd.concat(all_data, ignore_index=True))
+
+print(len(b), b.keys())
 
 # separate points into x,y
 b["X"] = b["geometry"].x
@@ -45,8 +70,8 @@ with open(baumfile,"w") as f:
 
 print("Trees written to ",baumfile)
 
-groups = b.loc[b["BAUMGRUPPE"] == "Baumgruppe"]
-singles = b.loc[b["BAUMGRUPPE"] == "Einzelbaum"]
+groups = b.loc[b["BAUMGRUPPE".lower()] == "Baumgruppe"]
+singles = b.loc[b["BAUMGRUPPE".lower()] == "Einzelbaum"]
 
 
 print("Groups: ", len(groups))
